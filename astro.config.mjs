f933b487e6099bf96f13622e0e4155d7143e68fc
Nils Haberkamp <haberkamp.n@gmail.com>
2025-02-14 16:00:00 +0100

    get rid of unnecessary dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel";
 
-import expressiveCode from "astro-expressive-code";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [icon(), expressiveCode(), react()],
 
   adapter: vercel(),
 });
